a034f3a346d70f4060079b87f9486aac78378970
WangQiang <157556212+Wangqiang-ECUT@users.noreply.github.com>
2025-01-18 11:00:41 +0800

    Add files via upload

---
+++ DnCNN_Denoised.py
@@ -0,0 +1,155 @@
+"""
+一维去噪 #音频大地电磁
+ DnCNN
+ """
+import torch  # 包 torch 包含了多维张量的数据结构以及基于其上的多种数学操作。
+import matplotlib.pyplot as plt  # 画图
+import numpy as np  # NumPy(Numerical Python) 是 Python 语言的一个扩展程序库，支持大量的维度数组与矩阵运算，此外也针对数组运算提供大量的数学函数库。
+
+# torch.nn，nn就是neural network的缩写，这是一个专门为深度学习而设计的模块。torch.nn的核心数据结构是Module，这是一个抽象的概念，
+# 既可以表示神经网络中的某个层（layer），也可以表示一个包含很多层的神经网络。在实际使用中，最常见的做法是继承nn.Module，撰写自己的网络/层。
+import torch.nn as nn
+
+import torch.optim as optim  # torch.optim是一个实现了各种优化算法的库。
+import time  # 记录整个训练时间
+import scipy.io as sio  # 加载.mat文件
+import math  # 数学运算
+from torch.utils.data import TensorDataset, DataLoader  # DataLoader类的作用就是实现数据以什么方式输入到什么网络中。
+from d2 import DnCNN  # 导入DnCNN模型
+#from d3 import ResNet18
+import gc  # 垃圾回收，请理内存
+from torchsummary import summary
+
+# 定义是否使用GPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # 判断gpu是否能用
+#net = DnCNN().to(device)  # 设置网络对象
+net = DnCNN().to(device)
+summary(net,input_size=(1, 75))
+#summary(ResNet18(), (1, 255, 255))
+#summary(DnCNN, input_size=[(1, 255 ,255)], batch_size=128, device="cpu")
+###超参数设置###
+start = time.process_time()
+EPOCH = 200  # 遍历数据集次数 100
+BATCH_SIZE_s = 8576# 批处理尺寸(batch_size)
+BATCH_SIZE_v = 8576 # 128
+LR = 0.00001  # 0.000001 学习率
+rate = 0.9  # 学习率衰变
+iteration = 10  # 每10次衰减
+a = torch.load(r'DnCNN_quzao_GPU.pth')
+###########################################################
+dataset_s = r'test_EY.mat'
+dataset_s = sio.loadmat(dataset_s)
+dataset_s = dataset_s['dataMatrix']  # 假设文件中存有字符变量是matrix，
+dataset_s = dataset_s[:, np.newaxis, :]
+
+Origin_v = dataset_s[0:8576, :, :]
+MTsignal_v = dataset_s[0:8576, :, :]
+Lv = 8576
+def normalization(data, _range):
+    return data / _range
+# 验证集归一化
+matirx_v = np.row_stack((Origin_v, MTsignal_v))
+range_v = np.max(abs(matirx_v))
+norm_v = normalization(matirx_v, range_v)
+np.save('range_v', range_v)
+print(range_v)
+Origin_v = norm_v[0:Lv, :]
+MTsignal_v = norm_v[Lv:Lv * 2, :]
+
+# 验证集格式转换
+x1_v = torch.from_numpy(Origin_v)
+x2_v = torch.from_numpy(MTsignal_v)
+x1_v = x1_v.type(torch.FloatTensor)
+x2_v = x2_v.type(torch.FloatTensor)
+# 数据封装打乱顺序
+val_data = TensorDataset(x2_v, x1_v)
+
+val_loader = DataLoader(val_data, batch_size=BATCH_SIZE_v, shuffle=True, num_workers=0, drop_last=False)
+# 变量清除
+del dataset_s
+# del Origin_s
+# del MTsignal_s
+#del dataset_v
+del Origin_v
+del MTsignal_v
+#del matirx_s
+#del norm_s
+del matirx_v
+del norm_v
+#del x1_s
+del x1_v
+#del x2_s
+del x2_v
+gc.collect()
+        # for j in range(Lv//BATCH_SIZE_v):
+for j, data_v in enumerate(val_loader, 0):
+    input_v, target_v = data_v
+    input_v = input_v.to(device)
+    output_v = a(input_v)  # 前向算法
+    target_v = target_v.to(device)
+###绘图###
+# 格式转换
+input_v = input_v.cpu()
+input_v = input_v.detach().numpy()
+output_v = output_v.cpu()
+output_v = output_v.detach().numpy()
+target_v = target_v.cpu()
+target_v = target_v.detach().numpy()
+#plt.plot(epoch, LR)  # 描述lr和epoch的关系
+#plt.show()
+#torch.load(net, 'net.pth')
+# 去噪前
+origSignal = target_v
+errorSignal = target_v - input_v
+signal_2 = sum(origSignal.flatten() ** 2)
+noise_2 = sum(errorSignal.flatten() ** 2)
+SNRValues3 = 10 * math.log10(signal_2 / noise_2)
+print(SNRValues3)
+
+# 去噪后
+origSignal = target_v
+errorSignal = target_v - output_v
+signal_2 = sum(origSignal.flatten() ** 2)
+noise_2 = sum(errorSignal.flatten() ** 2)
+SNRValues4 = 10 * math.log10(signal_2 / noise_2)
+print(SNRValues4)
+
+y1_list = []
+y2_list = []
+y3_list = []
+col = 0  # 显示第几个数据去噪效果
+while col <8576:
+      x = range(0, len(target_v[0, 0, :]))
+      y1 = target_v[col, 0, :]
+      y2 = input_v[col, 0, :]
+      y3 = output_v[col, 0, :]
+      plt.plot(x, y1, 'b.-')
+      plt.plot(x, y2, 'r.-')
+      plt.plot(x, y3, 'g.-')
+      plt.xlabel('Time')
+      plt.ylabel('Ampulitude')
+      origSignal = y1
+      errorSignal = y1 - y2
+      signal_2 = sum(origSignal.flatten() ** 2)  # **为幂计算，2**3=2的3次方，sum为求和，可叠代，flatten为降维为1维，按行的方向
+      noise_2 = sum(errorSignal.flatten() ** 2)  # 噪声
+      SNRValues1 = 10 * math.log10(signal_2 / noise_2)  # 信噪比计算公式
+      print(SNRValues1)  # 打印出信噪比的值
+
+      # 去噪后
+      origSignal = y1
+      errorSignal = y1 - y3
+      signal_2 = sum(origSignal.flatten() ** 2)  # 信号
+      noise_2 = sum(errorSignal.flatten() ** 2)  # 噪声
+      SNRValues2 = 10 * math.log10(signal_2 / noise_2)  # 信噪比计算公式
+      print(SNRValues2)  # 输出去噪后信噪比的值
+      # plt.show()
+      col += 1
+      y1_list.append(y1)
+      y2_list.append(y2)
+      y3_list.append(y3)
+
+np.savetxt(r"EY_tar.dat", y1_list, fmt='%10.4e')
+np.savetxt(r"EY_in.dat", y2_list, fmt='%10.4e')
+np.savetxt(r"EY_out.dat", y3_list, fmt='%10.4e')
+end = time.process_time()  # 记录时间的函数
+print(end - start)  # 打印出开始到结束的消耗时间
